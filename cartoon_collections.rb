400ef73776ef5979842cd2a643ef881efa2235f5
German Garcia <ggarciag.business@outlook.com>
2019-07-30 18:16:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+ require('pry')
+ def roll_call_dwarves(dwarfArray)
+    index = 0 
+    dwarfArray.each_with_index do |item, index| 
+      puts "#{index + 1} #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(source_array)
+    new = source_array.map {|name| name.capitalize}
+    new.map {|name|  "#{name}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(source_array)
+    source_array.map {|word| 
+      if word.length < 4 
+        return true
+      else
+        return false
+      end
+    }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(source_array)
+  
+  if source_array.include?("cheddar") == true
+    return "cheddar"
+  end
+  if source_array.include?("gouda") == true
+    return "gouda"
+  end  
 end
+
